904403b9bf7d78b75ba96527138a7603b4ca984c
barnali <barnalipaul890@gmail.com>
2024-11-11 20:33:08 +0530

    first commit

---
+++ mvc/views/pages/home.php
@@ -0,0 +1,19 @@
+<div  class="container-fluid p-0" >
+	<img id="banner" src="./public/images/banner/banner2.jpg" width="100%" style="margin-top: 85px">
+</div>
+
+<style type="text/css">
+	
+	background:#000000;
+	.post--box:hover{
+		transform: scale(1.02);
+		transition: 0.3s;
+	}
+</style>
+
+<div class="container pl-2 pr-2 pt-3 pb-5" style="background:#FFF2CC;">
+	<p class="font-weight-bold ml-1 mb-1" style="font-size: 180%">Popular right now </p>
+	<div class="row m-0">
+		<?php include("./mvc/views/partials/home-post.php"); ?>  
+	</div>
+</div>
